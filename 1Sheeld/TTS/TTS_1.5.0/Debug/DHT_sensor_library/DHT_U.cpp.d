99089070d41c287005dff43d9a5a8ce5e97684b1
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2018-11-16 19:40:29 +0100

    Add TTS 1.5.0
    Support for the DHT11 sensor and improvements

---
+++ 1Sheeld/TTS/TTS_1.5.0/Debug/DHT_sensor_library/DHT_U.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\Kacper\AppData\Local\Temp\VMBuilds\TTS\uno\Debug\DHT_sensor_library\DHT_U.cpp.o: \
+ C:\Users\Kacper\Documents\Arduino\libraries\DHT_sensor_library\DHT_U.cpp \
+ C:\Users\Kacper\Documents\Arduino\libraries\DHT_sensor_library\DHT_U.h \
+ C:\Users\Kacper\Documents\Arduino\libraries\Adafruit_Unified_Sensor/Adafruit_Sensor.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\Kacper\Documents\Arduino\libraries\DHT_sensor_library/DHT.h
---
+++ 1Sheeld/TTS/TTS_1.5.0/README.md
@@ -0,0 +1,23 @@
+# TTS 1.5.0
+
+(last update: 16.11.2018)
+
+
+<b>Source code in "TTS.ino"</b>
+
+I recommend using the 1Sheeld application on <b>iPhone</b> (better speech recognition)
+
+## Requires:
+
+* <b>1Sheeld</b>
+
+## Optional:
+
+* <b>DHT11 sensor</b>
+
+## Changes
+
+* <b>Support for the DHT11 sensor (command "DHT read")</b>
+
+* <b>Code optimization</b>
+* <b>Bug fixes</b>
